71e6a199546e520ae1d97507b8bd2ffebf7be6aa
devanshugupta24 <guptadevanshu24@gmail.com>
2025-01-03 21:27:40 +0530

    added a hr at start of footer, added navigate function in footer

--- frontend/src/components/Footer.components.jsx
+++ frontend/src/components/Footer.components.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-
+import { useNavigate } from 'react-router-dom'
 
 const Footer = () => {
+    const navigate=useNavigate()
   return (
-    <div className='md:mx-10 '>
-        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm '>
+    <div className='md:mx-10 mt-16'>
+        <hr />
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 text-sm '>
             {/*-----Left section-------*/}
             <div>
                 <img className='mb-5 w-40'src={assets.logo} alt="" />
@@ -16,9 +18,9 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>COMPANY</p>
                 <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact us</li>
+                    <li className='cursor-pointer hover:text-gray-700 hover:scale-105 transition-all'onClick={()=> {navigate('/');scrollTo(0,0)}}>Home</li>
+                    <li className='cursor-pointer hover:text-gray-700 hover:scale-105 transition-all'onClick={()=> {navigate('/about');scrollTo(0,0)}}>About us</li>
+                    <li className='cursor-pointer hover:text-gray-700 hover:scale-105 transition-all'onClick={()=> {navigate('contact');scrollTo(0,0)}}>Contact us</li>
                     <li>Privacy policy</li>
                 </ul>
             </div>
