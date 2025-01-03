332d7403029ffa123b162e95474492bbd0157ba3
devanshugupta24 <guptadevanshu24@gmail.com>
2025-01-03 21:34:11 +0530

    added navigate to homepage after clicking on create account or login in login page

--- frontend/src/pages/Login.pages.jsx
+++ frontend/src/pages/Login.pages.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-
+  const navigate=useNavigate()
   const [state,setState]=useState('Sign Up')
   const [email,setEmail]=useState('')
   const [password,setPassword] =useState('')
@@ -30,7 +31,7 @@ const Login = () => {
           <p>Password</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" onChange={(e)=>setPassword(e.target.value)} value={password} required />
         </div>
-        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state==='Sign Up' ? "Create Account" : "Login"}</button>
+        <button onClick={()=>navigate('/')} className='bg-primary text-white w-full py-2 rounded-md text-base'>{state==='Sign Up' ? "Create Account" : "Login"}</button>
         {
           state==="Sign Up"
           ?<p>Already have an account? <span onClick={()=>setState('Login')}className='text-primary underline cursor-pointer'>Login here</span></p>
