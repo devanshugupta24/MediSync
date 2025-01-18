10cbb20b533e5abcd9ace4e06b5752f6df3984bb
devanshugupta24 <guptadevanshu24@gmail.com>
2025-01-18 12:00:12 +0530

    added appointment booking functionality with API and frontend integration

--- frontend/src/pages/Appointment.pages.jsx
+++ frontend/src/pages/Appointment.pages.jsx
@@ -1,18 +1,21 @@
 import React, { useContext, useEffect, useState } from 'react'
-import {useParams} from 'react-router-dom'
+import {useNavigate, useParams} from 'react-router-dom'
 import {AppContext} from '../context/AppContext'
 import { assets } from '../assets/assets'
 import RelatedDoctors from '../components/relatedDoctors.components'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 
 const Appointment = () => {
 
   const {docId}=useParams()  // Extracts the docId parameter from the route URL.
-  const {doctors,currencySymbol}=useContext(AppContext)
+  const {doctors,currencySymbol,backendUrl,token,getDoctorsData}=useContext(AppContext)
 
   const daysOfWeek=['SUN','MON','TUE','WED','THU','FRI','SAT']
 
-  const [docInfo,setDocInfo]=useState(null)
+  const navigate=useNavigate()
+  const [docInfo,setDocInfo]=useState(false)
   const [docSlots,setDocSlots]=useState([])
   const [slotIndex,setSlotIndex]=useState(0)
   const [slotTime,setSlotTime]=useState('')
@@ -23,11 +26,12 @@ const Appointment = () => {
   }
 
   const getAvailableSlots = async () => {
+    if (!docInfo) return; // Ensure docInfo is not null before proceeding.
     setDocSlots([]); // Clears the docSlots state to prepare for new data.
   
     let today = new Date(); // creates a today object to get current date and time(Date contains real-time value)
     let isTodayPastEndTime = today.getHours() >= 21; // Check if the current time is past 21:00. If true, skip today's slots.
-    let firstSlotIndex = 0; // To store the index of the first available slot (used for default selection).
+    let firstSlotIndex = -1; // To store the index of the first available slot (used for default selection).
   
     for (let i = 0; i < 7; i++) {
       // getting date with index
@@ -61,11 +65,25 @@ const Appointment = () => {
         // jbtk 9 naa baj jai tbtk
         let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); // used to format the syntax of time
   
-        // add slot to array
-        timeSlots.push({
-          datetime: new Date(currentDate),
-          time: formattedTime,
-        });
+        let day = currentDate.getDate()
+        let month  = currentDate.getMonth()+1
+        let year = currentDate.getFullYear()
+
+        const slotDate=day+"_"+month +"_"+year
+        const slotTime=formattedTime
+
+        
+        const isSlotAvailable = docInfo.slots_booked[slotDate] && docInfo.slots_booked[slotDate].includes(slotTime) ? false : true
+        
+
+        if(isSlotAvailable){
+          // add slot to array
+          timeSlots.push({
+            datetime: new Date(currentDate),
+            time: formattedTime
+          })
+        }
+
   
         // increment current time by 30mins
         currentDate.setMinutes(currentDate.getMinutes() + 30);
@@ -84,21 +102,51 @@ const Appointment = () => {
   };
   
   
+  const bookAppointment=async()=>{
+    if(!token){
+      toast.warn('Login to book appointment')
+      return navigate('/login')
+    }
+
+    try {
+      const date=docSlots[slotIndex][0].datetime
+
+      let day=date.getDate()
+      let month=date.getMonth()+1
+      let year=date.getFullYear()
+
+      const slotDate=day+"_"+month +"_"+year
+
+      const {data}=await axios.post(backendUrl+'/api/user/book-appointment',{docId,slotDate,slotTime},{headers:{token}})
+      if(data.success){
+        toast.success(data.message)
+        getDoctorsData()
+        navigate('/my-appointments')
+      }else{
+        toast.error(data.message)
+      }
+      
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
 
     useEffect(()=>{
-      fetchDocInfo()
+      if(doctors.length>0){
+        fetchDocInfo()
+      }
     },[doctors,docId])
 
     useEffect(()=>{
-      getAvailableSlots()
-    },[docId])
-
-    useEffect(()=>{
-      console.log(docSlots)
-    },[docSlots])
+      if(docInfo){
+        getAvailableSlots()
+      }
+    },[docInfo])
 
 
-  return docInfo &&(
+
+  return docInfo && (
     <div>
       {/*---------doctors details---------*/ }
       <div className='flex flex-col sm:flex-row gap-4'>
@@ -147,7 +195,7 @@ const Appointment = () => {
             </p>
           ))}
         </div>
-        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6 hover:bg-indigo-600 transition-all duration-200'>Book an Appointment</button>
+        <button onClick={bookAppointment} className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6 hover:bg-indigo-600 transition-all duration-200'>Book an Appointment</button>
       </div>
       {/*-----listing related doctors------*/}
       <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
