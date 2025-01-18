aa48770a8353e0e93b834e84dc1a12dfc4644ea6
devanshugupta24 <guptadevanshu24@gmail.com>
2025-01-18 19:45:51 +0530

    added cancel appointment functionality with backend logic and frontend integration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import { v2 as cloudinary } from 'cloudinary';
 import doctorModel from '../models/doctorModel.js';
 import appointmentModel from '../models/appointmentModel.js';
+import razorpay from 'razorpay'
 
 
 //API to register User
@@ -217,4 +218,45 @@ const listAppointment=async(req,res)=>{
 }
 
 
-export { registerUser ,loginUser,getProfile,updateProfile,bookAppointment,listAppointment}
+//Logic to cancel the appointment
+const cancelAppointment= async(req,res)=>{
+    try {
+        const {userId,appointmentId}= req.body;
+        const appointmentData=await appointmentModel.findById(appointmentId)
+        //verify appointment user
+
+        if(appointmentData.userId!==userId){
+            return res.json({success:false,message:'Unauthorized action'})
+        }
+
+        await appointmentModel.findByIdAndUpdate(appointmentId,{cancelled:true})
+
+        //releasing doctor slot
+         
+        const {docId,slotDate,slotTime} = appointmentData
+
+        const doctorData= await doctorModel.findById(docId)
+
+        let slots_booked=doctorData.slots_booked
+
+        slots_booked[slotDate]= slots_booked[slotDate].filter(e=>e!==slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId,{slots_booked})
+
+        res.json({success:true,message:'Appointment cancelled'})
+
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message: error.message });
+    }
+}
+
+
+
+//API to make payment using Razorpay
+
+
+
+
+export { registerUser ,loginUser,getProfile,updateProfile,bookAppointment,listAppointment ,cancelAppointment}
