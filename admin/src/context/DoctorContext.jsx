deba83763aac0fdf52f073613fe9f838048e2ce6
devanshugupta24 <guptadevanshu24@gmail.com>
2025-01-22 00:21:34 +0530

    Add doctor profile management functionality with update and retrieval APIs

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -12,6 +12,8 @@ const DoctorContextProvider = (props) => {
     const [dToken,setDToken]=useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):'')
     const [appointments,setAppointments] =useState([])
     const [dashData,setDashData]= useState(false)
+    const [profileData,setProfileData]= useState(false)
+
     const getAppointments=async()=>{
         try {
             const {data}= await axios.get(backendUrl+'/api/doctor/appointments',{headers:{dToken}})
@@ -81,6 +83,20 @@ const DoctorContextProvider = (props) => {
     }
 
 
+    const getProfileData=async()=>{
+        try {
+            const {data}= await axios.get(backendUrl+'/api/doctor/profile',{headers:{dToken}})
+            if(data.success){
+                setProfileData(data.profileData)
+                
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+
 
     const value={
         dToken,setDToken,
@@ -88,7 +104,8 @@ const DoctorContextProvider = (props) => {
         getAppointments,appointments,setAppointments,
         completeAppointment,
         cancelAppointment,
-        getDashData,dashData,setDashData
+        getDashData,dashData,setDashData,
+        profileData,setProfileData,getProfileData
         
     }
 
--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -160,4 +160,42 @@ const doctorDashboard=async(req,res)=>{
 }
 
 
-export {changeAvailability,doctorList,loginDoctor,appointmentsDoctor,appointmentCancel,appointmentComplete,doctorDashboard}
+//API to get doctor profile to get doctor panel
+const doctorProfile=async(req,res)=>{
+    try {
+        const {docId}= req.body
+        const profileData=await doctorModel.findById(docId).select("-password")
+
+        res.json({success:true,profileData})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+
+//API to update profile data from doctor panel
+const updateDoctorProfile=async(req,res)=>{
+    try {
+        const {docId,fees,address,available}= req.body
+        await doctorModel.findByIdAndUpdate(docId,{fees,address,available})
+        res.json({success:true,message:"Profile Updated"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+
+
+export {changeAvailability,
+    doctorList,
+    loginDoctor,
+    appointmentsDoctor,
+    appointmentCancel,
+    appointmentComplete,
+    doctorDashboard,
+    updateDoctorProfile,
+    doctorProfile
+}
