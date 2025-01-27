d643aa14c46fa786bc29f35cc5cfa93e8248bdf0
devanshugupta24 <guptadevanshu24@gmail.com>
2025-01-28 00:53:14 +0530

    updated minor errors

--- frontend/src/pages/Contact.pages.jsx
+++ frontend/src/pages/Contact.pages.jsx
@@ -11,8 +11,8 @@ const Contact = () => {
         <img className='w-2/3 md:max-w-[340px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col justify-center items-center md:items-start gap-4'>
           <p className='font-semibold text-lg text-gray-600'>Our Office</p>
-          <p className='text-gray-500 text-center md:text-start'>Bhabua kaimur <br />bihar 821101</p>
-          <p className='text-gray-500 text-center md:text-start'>+91 90037208420 <br />email:devasggu@gmail.com</p>
+          <p className='text-gray-500 text-center md:text-start'>Bhabua kaimur <br />821101 , bihar</p>
+          <p className='text-gray-500 text-center md:text-start'>+91 9142088391 <br />email : guptadevanshu24@gmail.com</p>
           <p className='font-semibold text-lg text-gray-600'>Careers at MediSync</p>
           <p className='text-gray-500'>learn more about our jobs and team openings</p>
           <button className='border border-black px-4 py-2 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore jobs</button>
