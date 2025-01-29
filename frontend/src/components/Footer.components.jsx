80d5c9353094d466fe3d5a4404471cf4810e763a
devanshugupta24 <guptadevanshu24@gmail.com>
2025-01-29 18:49:07 +0530

    Update contact phone number in Footer and Contact pages

--- frontend/src/components/Footer.components.jsx
+++ frontend/src/components/Footer.components.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
                 <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>+91 9095032995</li>
+                    <li>+91 9999999999</li>
                     <li>guptadevanshu24@gmail.com</li>
                 </ul>
             </div>
