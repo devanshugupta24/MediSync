9e9f153147bf72ff1f7ef51bc21bd7b4d94a8ec2
devanshugupta24 <guptadevanshu24@gmail.com>
2025-02-02 00:07:06 +0530

    update Flask app to use environment variable for port configuration

--- backendml/app.py
+++ backendml/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import joblib
 import numpy as np
 from flask_cors import CORS
@@ -35,4 +36,5 @@ def predict_heart():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
